0f8b5230aec1158b679ccf304b01cefe89318600
alexsilva <geniofuturo@gmail.com>
2018-11-12 20:30:15 -0200

    Setup slackbot loggers.

--- slack_sync/management/commands/slackbot_run.py
+++ slack_sync/management/commands/slackbot_run.py
@@ -1,4 +1,7 @@
 # coding=utf-8
+import logging
+import sys
+
 from django.core.management import BaseCommand
 from django.utils.translation import gettext_lazy as _
 
@@ -9,7 +12,29 @@ from slack_sync.signals import before_bot_run
 class Command(BaseCommand):
     help = _("""Slackboot Server""")
 
+    LOGGER_FORMAT = '[%(asctime)s: %(levelname)s/%(name)s] %(message)s'
+
+    @classmethod
+    def setup_loggers(cls):
+        """slackbot logger setup"""
+        shandler = logging.StreamHandler(sys.stdout)
+        shandler.setLevel(logging.DEBUG)
+
+        # create formatter
+        formatter = logging.Formatter(cls.LOGGER_FORMAT)
+
+        # add formatter to ch
+        shandler.setFormatter(formatter)
+
+        # add ch to logger
+        for name in ('slackbot.slackclient',
+                     'slackbot.bot'):
+            logger = logging.getLogger(name)
+            logger.setLevel(logging.DEBUG)
+            logger.addHandler(shandler)
+
     def handle(self, *args, **options):
+        self.setup_loggers()
         # slackbot server
         bot = Bot()
         before_bot_run.send(sender=self.__class__,
