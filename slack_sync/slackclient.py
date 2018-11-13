8d008215dac78334b80e499d59680b2442202360
alexsilva <geniofuturo@gmail.com>
2018-11-13 11:47:40 -0200

    Adding api method 'send_user_message' for sending messages by email.

--- slack_sync/slackclient.py
+++ slack_sync/slackclient.py
@@ -1,4 +1,6 @@
+from six import iterkeys
 from slackbot import slackclient
+from slackbot.dispatcher import unicode_compact
 
 from .signals import after_client_connection
 
@@ -17,3 +19,26 @@ class SlackClient(slackclient.SlackClient):
         except Exception:
             slackclient.logger.exception("signal after-client-connection")
         return retval
+
+    def open_dm_channel(self, user_id):
+        return self.webapi.im.open(user_id).body["channel"]["id"]
+
+    def find_user_by_email(self, email):
+        for userid in iterkeys(self.users):
+            profile = self.users[userid]['profile']
+            if 'email' in profile and profile['email'] == email:
+                return userid
+        raise ValueError("email not found!")
+
+    @unicode_compact
+    def send_user_message(self, email, text, **kwargs):
+        """
+            Sends messages to a user by their email.
+            Raises ValueError if email does not exist and errors is True.
+        """
+        try:
+            channel_id = self.open_dm_channel(self.find_user_by_email(email))
+            self.rtm_send_message(channel_id, text)
+        except ValueError:
+            if kwargs.get("errors", False):
+                raise
