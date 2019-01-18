5a860131dfbe56e8a062006bdd74d01836e168cd
alexsilva <geniofuturo@gmail.com>
2019-01-18 16:35:21 -0200

    v1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-slack-sync',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     url='https://github.com/alexsilva/django-slack-sync',
     license='MIT',
